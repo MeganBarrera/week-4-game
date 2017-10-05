9f37616d22fe0363f380faa22c22dbc5b048e23d
MeganBarrera <megan.barrera1@gmail.com>
2017-10-05 16:37:11 -0500

    second commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+var wins = 0;
+var losses = 0;
+
+function {
+	var min = 19
+	var max = 120
+
+	var randomNumber = Math.floor(Math.random() * 10) + 18;
+}
+
+	var min = 1
+	var max = 12
+
+	var randomNumber = Math.floor(Math.random() * 12);
